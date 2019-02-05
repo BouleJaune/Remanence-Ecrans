011c2fa8c0f58268cef78cd38544bc44c27531dc
thierry amettler <amthierry81@live.fr>
2019-02-05 20:37:36 +0100

    new file:   Code arduino/TDRTimerBin/TDRTimerBin.ino 	new file:   Code arduino/TDRTimerBin/hs_err_pid7740.log 	new file:   Code arduino/TDRtimer/TDRtimer.ino

---
+++ Code arduino/TDRTimerBin/TDRTimerBin.ino
@@ -0,0 +1,28 @@
+#include <FlexiTimer2.h>
+
+
+    int photocellPin = 0;     
+    int photocellReading;     
+    byte lb;
+    byte hb;
+    
+    void setup(void) {
+      
+      Serial.begin(115200);
+      FlexiTimer2::set(1,1.0/2000, Mesure); 
+      FlexiTimer2::start(); 
+    }
+     
+    void loop(void) {
+      
+    }
+
+    void Mesure() {
+      photocellReading = analogRead(photocellPin);   
+      // Serial.write(photocellPin);  
+      lb=lowByte(photocellReading);
+      hb=highByte(photocellReading);
+      Serial.write(lb);
+      Serial.write(hb);       
+    }
+
---
+++ Code arduino/TDRtimer/TDRtimer.ino
@@ -0,0 +1,29 @@
+#include <FlexiTimer2.h>
+
+
+    int photocellPin = 0;     
+    int photocellReading;     
+    byte lb;
+    byte hb;
+    
+    void setup(void) {
+      
+      Serial.begin(9600);
+      FlexiTimer2::set(1,1.0/2000, Mesure); 
+      FlexiTimer2::start(); 
+    }
+     
+    void loop(void) {
+      
+    }
+
+    void Mesure() {
+      photocellReading = analogRead(photocellPin);   
+	    Serial.print(photocellReading);	
+      Serial.print("\n");
+      // lb=lowByte(photocellReading);
+      // hb=highByte(photocellReading);
+      // Serial.write(lb);
+      // Serial.write(hb);       
+    }
+
